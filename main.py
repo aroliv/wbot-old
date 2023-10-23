f48eefb519aee671c769cce4bfb0c82a64a52ef2
Andressa Ribeiro <andressaribeiro@usp.br>
2023-10-23 15:26:36 -0300

    Initial commit

--- main.py
+++ main.py
@@ -25,21 +25,25 @@ def webhook():
         start_conversation()
     else:
         if "e você?" in user_message or "e vc?" in user_message or "e vc" in user_message:
+            message.media("https://github.com/aroliv/whatsapp-bot-old/blob/main/data/img/Velho.png?raw=true")
             message.body("Dado que sou tão somente um bot, creio que não consiga responder corretamente esta pergunta.")
         elif "fone" in user_message or "fone de ouvido" in user_message:
+            message.media("https://github.com/aroliv/whatsapp-bot-old/blob/main/data/img/Velho.png?raw=true")
             message.body("Entendo que você esteja com problemas com fones de ouvido. Pode nos contar mais sobre o problema?")
-        elif "quebrou" in user_message or "direito" in user_message:
+        elif "quebrou" in user_message or "direito" in user_message or "defeito" in user_message:
             message.body("Preciso localizar seu pedido. Pode me informar seu nome? Preciso que você mande no formato 'Nome: Fulano Oliveira'")
         elif "Nome" in user_message or "nome" in user_message:
+            message.media("https://github.com/aroliv/whatsapp-bot-old/blob/main/data/img/Velho.png?raw=true")
             message.body("Obrigado por me informar seu nome. Pode me informar seu e-mail? Preciso que você mande no formato 'E-mail: fulanooliveira@gmail.com'")
         elif '@' in user_message:
+            message.media("https://github.com/aroliv/whatsapp-bot-old/blob/main/data/img/Velho.png?raw=true")
             message.body("Obrigado por fornecer as informações. Parece que o problema ocorreu no seu último pedido. Você prefere que seja devolvido em uma agência próxima ou que os Correios retirem na sua casa?")
         elif "agência" in user_message or "casa" in user_message or "agencia" in user_message:
-            message.body("Perfeito! Seu código de rastreio é: NB123456BR. Mais informações serão enviadas por e-mail! Nossa interação se encerra por aqui. Volte para o Qualtrics e continue com a pesquisa.")
-            # Envie uma imagem como resposta
             message.media("https://github.com/aroliv/whatsapp-bot-old/blob/main/data/img/Velho.png?raw=true")
+            message.body("Perfeito! Seu código de rastreio é: NB123456BR. Mais informações serão enviadas por e-mail! Nossa interação se encerra por aqui. Volte para o Qualtrics e continue com a pesquisa.")
             bot_state.clear()
         else:
+            message.media("https://github.com/aroliv/whatsapp-bot-old/blob/main/data/img/Velho.png?raw=true")
             message.body("Desculpe, não entendi. Como posso ajudar?")
     
     return str(response)
--- utils.py
+++ utils.py
@@ -10,9 +10,17 @@ def detect_intent_from_text(text, session_id, language_code='pt-br'):
     text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.types.QueryInput(text=text_input)
     response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
-    return response.query_result
+    return response
 
+def extract_media_url(response):
+    # Extraia a URL da mídia da resposta do Dialogflow
+    if response.query_result.webhook_payload and "media-url" in response.query_result.webhook_payload:
+        return response.query_result.webhook_payload["media-url"]
+    return None
 
 def fetch_reply(query, session_id):
-	response = detect_intent_from_text(query,session_id)
-	return response.fulfillment_text
+    response = detect_intent_from_text(query, session_id)
+    fulfillment_text = response.query_result.fulfillment_text
+    media_url = extract_media_url(response)
+
+    return fulfillment_text, media_url
